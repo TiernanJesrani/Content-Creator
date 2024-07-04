28066d26795a13fc1ad929dd0cb06be51029e0cc
TiernanJesrani <134875734+TiernanJesrani@users.noreply.github.com>
2024-07-03 22:56:44 -0400

    added subtitles to video

--- Edit/combine.py
+++ Edit/combine.py
@@ -1,48 +1,67 @@
 import ffmpeg
 import requests
 import json
+import os
 from eleven_keys import XI, ANTONI
 # Define constants for the script
 CHUNK_SIZE = 1024  # Size of chunks to read/write at a time
 XI_API_KEY = XI  # Your API key for authentication
 VOICE_ID = ANTONI # ID of the voice model to use
-TEXT_TO_SPEAK = "Am I the asshole for being a baby waby daby? Yes I am."  # Text you want to convert to speech
+TEXT_TO_SPEAK = "Am I the asshole for saying it's my birthday over and over again?"  # Text you want to convert to speech
 OUTPUT_PATH = "output.mp3"  # Path to save the output audio file
 
-# Construct the URL for the Text-to-Speech API request
-tts_url = f"https://api.elevenlabs.io/v1/text-to-speech/{VOICE_ID}/stream"
-
-# Set up headers for the API request, including the API key for authentication
-headers = {
-    "Accept": "application/json",
-    "xi-api-key": XI_API_KEY
-}
-
-# Set up the data payload for the API request, including the text and voice settings
-data = {
-    "text": TEXT_TO_SPEAK,
-    "model_id": "eleven_multilingual_v2",
-    "voice_settings": {
-        "stability": 0.5,
-        "similarity_boost": 0.8,
-        "style": 0.0,
-        "use_speaker_boost": True
+def create_audio(text, out_path):
+    tts_url = f"https://api.elevenlabs.io/v1/text-to-speech/{VOICE_ID}/stream"
+    headers = {
+        "Accept": "application/json",
+        "xi-api-key": XI_API_KEY
     }
-}
+    data = {
+        "text": text,
+        "model_id": "eleven_multilingual_v2",
+        "voice_settings": {
+            "stability": 0.5,
+            "similarity_boost": 0.8,
+            "style": 0.0,
+            "use_speaker_boost": True
+        }
+    }
 
-# Make the POST request to the TTS API with headers and data, enabling streaming response
-response = requests.post(tts_url, headers=headers, json=data, stream=True)
-
-# Check if the request was successful
-if response.ok:
+    response = requests.post(tts_url, headers=headers, json=data, stream=True)
+    
+    if response.ok:
     # Open the output file in write-binary mode
-    with open(OUTPUT_PATH, "wb") as f:
+        with open(out_path, "wb") as f:
         # Read the response in chunks and write to the file
-        for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
-            f.write(chunk)
-    # Inform the user of success
-    print("Audio stream saved successfully.")
-else:
+            for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+                f.write(chunk)
+        # Inform the user of success
+        print("Audio stream saved successfully.")
+    else:
     # Print the error message if the request was not successful
-    print(response.text)
+        print(response.text)
 
+def merge_audio(audio_path, video_path, out_path):
+    # get the duration of the audio file
+    duration = ffmpeg.probe(audio_path)["format"]["duration"]
+    # trim the video file to match the duration of the audio file
+    ffmpeg.input(video_path).output("tma.mp4", t=duration).run()
+    #get both to be combined
+    input_video = ffmpeg.input("tma.mp4")
+    added_audio = ffmpeg.input(audio_path).audio #.filter('adelay', "1500|1500")
+    # combine the two inputs
+    (
+        ffmpeg
+        .concat(input_video, added_audio, v=1, a=1)
+        .output(out_path)
+        .run(overwrite_output=True)
+    )   
+    if os.path.exists("tma.mp4"):
+        os.remove ("tma.mp4")
+
+def main():
+    #create_audio(TEXT_TO_SPEAK, "testing_audio.mp3")
+    merge_audio("testing_audio.mp3", "test_2.mp4", "testing_video.mp4")
+
+if __name__ == "__main__":
+    main()
--- Edit/subs.py
+++ Edit/subs.py
@@ -1,15 +1,25 @@
-from moviepy.editor import *
-from moviepy.video.tools.subtitles import SubtitlesClip
+from moviepy import editor
+TEXT_TO_SPEAK = "Am I the asshole for saying it's my birthday over and over again?"
 
-generator = lambda txt: TextClip(txt, font='Arial', fontsize=24, color='white')
-subs = [((0, 4), 'subs1'),
-        ((4, 9), 'subs2'),
-        ((9, 12), 'subs3'),
-        ((12, 16), 'subs4')]
+def annotate(clip, txt, txt_color='black', fontsize=50, font='Chalkboard'):
+    txtclip = editor.TextClip(txt, fontsize=fontsize, font=font, color=txt_color, stroke_color='white', stroke_width=2)
+    cvc = editor.CompositeVideoClip([clip, txtclip.set_pos(('center', 'center'))])
+    return cvc.set_duration(clip.duration)
 
-subtitles = SubtitlesClip(subs, generator)
+def create_subs(txt):
+    subs = []
+    i = 0
+    for word in txt.split():
+        subs.append(((i, i + 0.2), word))
+        i = i + 0.2
+    return subs
 
-video = VideoFileClip("videoplayback-2.mp4")
-result = CompositeVideoClip([video, subtitles.set_pos(('center','bottom'))])
+def main():
+    video = editor.VideoFileClip("vid.mp4")
+    subs = create_subs(TEXT_TO_SPEAK)
+    annotated_clips = [annotate(video.subclip(from_t, to_t), txt) for (from_t, to_t), txt in subs]
+    final_clip = editor.concatenate_videoclips(annotated_clips)
+    final_clip.write_videofile("test_2.mp4")
 
-result.write_videofile("output.mp4", fps=video.fps, temp_audiofile="temp-audio.m4a", remove_temp=True, codec="libx264", audio_codec="aac")
+if __name__ == "__main__":
+    main()
